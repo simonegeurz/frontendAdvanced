2231a0948281651e1c797078534462d859faff97
Geurtz,Simone S <s.geurtz@student.fontys.nl>
2023-04-06 09:15:22 +0200

    first draft

---
+++ individual-frontend/src/components/feed/feed.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../sass/_feedpage.css";
+
+function feed() {
+    return (
+        <div className="fullPage">
+            <p>Empty feed</p>    
+        </div>
+    );
+}
+
+export default feed;
